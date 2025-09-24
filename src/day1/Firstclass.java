f1c584de402b1cb52fc268dd11a4ad7af911b277
kurmareshma <kurmareshma123@gmail.com>
2025-09-24 06:58:57 +0530

    fist time pushing

--- src/day1/Firstclass.java
+++ src/day1/Firstclass.java
@@ -5,7 +5,8 @@ public class Firstclass
 public static void main(String[] args) 
 		{
 	System.out.println("hello india");
-	System.out.println("welcome to hyderabad");		
+	System.out.println("welcome to hyderabad");
+	
 		}
 
 }
