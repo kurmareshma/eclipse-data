91f357cce77a430a169bcab4ca676dca2e5f2b6f
kurmareshma <kurmareshma123@gmail.com>
2025-09-25 07:16:20 +0530

    create a new branch for negitive cases

---
+++ src/facebooknegitive/Fbloginwithspecialcharacters.java
@@ -0,0 +1,13 @@
+package facebooknegitive;
+
+public class Fbloginwithspecialcharacters 
+{
+
+	public static void main(String[] args) 
+	{
+      System.out.println("user able to enter the user name as 1234##1234");
+      System.out.println("user able to enter the password  as +-_+-");
+      System.out.println("user able to click on login");
+	}
+
+}
