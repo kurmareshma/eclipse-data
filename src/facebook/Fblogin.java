9f85f383c4f5b4ab2bd0fb3039cc643e888ca3d3
kurmareshma <kurmareshma123@gmail.com>
2025-09-25 07:02:35 +0530

    new package added with loginmodule and before that we created new branch

---
+++ src/facebook/Fblogin.java
@@ -0,0 +1,12 @@
+package facebook;
+
+public class Fblogin 
+{
+ public static void main(String[] args) 
+ {
+	System.out.println("user able to enter user name");  
+	System.out.println("user able to enter user password");  
+	System.out.println("user able to enter login");  
+		
+}
+}
